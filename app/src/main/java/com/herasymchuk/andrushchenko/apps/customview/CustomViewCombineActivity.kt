bd3f428ab27981f598af8a3c5e8b90addba4a2c9
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-01-18 16:00:43 +0200

    feat: use the custom view and it's methods in Activity

--- app/src/main/java/com/herasymchuk/andrushchenko/apps/customview/CustomViewCombineActivity.kt
+++ app/src/main/java/com/herasymchuk/andrushchenko/apps/customview/CustomViewCombineActivity.kt
@@ -1,6 +1,9 @@
 package com.herasymchuk.andrushchenko.apps.customview
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.herasymchuk.andrushchenko.databinding.ActivityCustomViewCombineBinding
@@ -9,6 +12,8 @@ import com.herasymchuk.andrushchenko.insets.applyInsets
 class CustomViewCombineActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCustomViewCombineBinding
+    private val token = Any()
+    private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +24,28 @@ class CustomViewCombineActivity : AppCompatActivity() {
         binding.root.applyInsets(top = true, left = true, right = true, bottom = true)
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.bottomButtons.setPositiveButtonListener {
+            binding.bottomButtons.isProgressMode = true
+            handler.postDelayed({
+                binding.bottomButtons.isProgressMode = false
+                binding.bottomButtons.setPositiveButtonText("Updated Apply")
+                Toast.makeText(this, "Positive button pressed", Toast.LENGTH_SHORT).show()
+            }, token, 2000)
+        }
+        binding.bottomButtons.setNegativeButtonListener {
+            binding.bottomButtons.setNegativeButtonText("Updated Cancel")
+            Toast.makeText(this, "Negative button pressed", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressedDispatcher.onBackPressed()
         return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(token)
     }
 }
