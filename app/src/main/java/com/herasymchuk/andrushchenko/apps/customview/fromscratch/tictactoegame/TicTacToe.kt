19c2d69e43cfa5d5f409f76252b2ac6368c67451
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-01-30 20:48:01 +0200

    refactor: make TicTacToe logic more OOP

--- app/src/main/java/com/herasymchuk/andrushchenko/apps/customview/fromscratch/tictactoegame/TicTacToe.kt
+++ app/src/main/java/com/herasymchuk/andrushchenko/apps/customview/fromscratch/tictactoegame/TicTacToe.kt
@@ -1,32 +1,46 @@
 package com.herasymchuk.andrushchenko.apps.customview.fromscratch.tictactoegame
 
-interface Board {
-    fun getCell(position: Position): Cell
-    fun setCell(position: Position, cell: Cell): Boolean
+fun interface FieldObserver {
+    fun onFieldChanged(field: List<List<Cell>>)
 }
 
-data class Position(
-    val row: Int,
-    val column: Int,
-    private val maxRow: Int,
-    private val maxColumn: Int
-) {
-    init {
-        if (row < 0 || row >= maxRow || column < 0 || column >= maxColumn) {
-            throw IllegalStateException("Row or column is out of bounds")
-        }
-    }
+interface Board {
+    fun getCell(position: TicTacToe.Position): Cell
+
+    fun setCell(position: TicTacToe.Position, cell: Cell): Boolean
+    val field: List<List<Cell>>
 }
 
 class TicTacToe(private val rows: Int, private val columns: Int) : Board {
+    override val field: MutableList<MutableList<Cell>> = MutableList(rows) { MutableList(columns) { Cell.EMPTY } }
+    private val observers: MutableSet<FieldObserver> = mutableSetOf()
 
-    private val field = Array(rows) { Array(columns) { Cell.EMPTY } }
-    override fun getCell(position: Position): Cell {
+    override fun getCell(position: TicTacToe.Position): Cell {
         return field[position.row][position.column]
     }
 
-    override fun setCell(position: Position, cell: Cell): Boolean {
+    override fun setCell(position: TicTacToe.Position, cell: Cell): Boolean {
         field[position.row][position.column] = cell
+        observers.forEach { it.onFieldChanged(field) }
         return true
+    }
+
+    fun subscribe(fieldObserver: FieldObserver) {
+        observers.add(fieldObserver)
+    }
+
+    fun unsubscribe(fieldObserver: FieldObserver) {
+        observers.remove(fieldObserver)
+    }
+
+    inner class Position(
+        val row: Int,
+        val column: Int
+    ) {
+        init {
+            if (row < 0 || row >= rows || column < 0 || column >= columns) {
+                throw IllegalStateException("Row or column is out of bounds")
+            }
+        }
     }
 }
--- app/src/main/java/com/herasymchuk/andrushchenko/apps/customview/fromscratch/ui/TicTacToeView.kt
+++ app/src/main/java/com/herasymchuk/andrushchenko/apps/customview/fromscratch/ui/TicTacToeView.kt
@@ -10,10 +10,4 @@ class TicTacToeView @JvmOverloads constructor(
     attrs: AttributeSet?,
     defStyleAttr: Int = R.attr.ticTackToeFieldStyle,
     defStyleRes: Int = R.style.DefaultTicTacToeFieldStyle
-) : View(context, attrs, defStyleAttr, defStyleRes) {
-
-    init {
-
-    }
-
-}
+) : View(context, attrs, defStyleAttr, defStyleRes)
