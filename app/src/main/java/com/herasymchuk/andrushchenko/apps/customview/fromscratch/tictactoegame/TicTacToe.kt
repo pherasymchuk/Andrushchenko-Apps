e7d87f258bfe2f632d6056d8745da5827b7fd8b0
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-01-30 21:08:34 +0200

    refactor: add more abstractions

--- app/src/main/java/com/herasymchuk/andrushchenko/apps/customview/fromscratch/tictactoegame/TicTacToe.kt
+++ app/src/main/java/com/herasymchuk/andrushchenko/apps/customview/fromscratch/tictactoegame/TicTacToe.kt
@@ -4,34 +4,40 @@ fun interface FieldObserver {
     fun onFieldChanged(field: List<List<Cell>>)
 }
 
-interface Board {
-    fun getCell(position: TicTacToe.Position): Cell
-
-    fun setCell(position: TicTacToe.Position, cell: Cell): Boolean
-    val field: List<List<Cell>>
+interface ObservableField {
+    fun subscribe(observer: FieldObserver)
+    fun unsubscribe(observer: FieldObserver)
 }
 
-class TicTacToe(private val rows: Int, private val columns: Int) : Board {
-    override val field: MutableList<MutableList<Cell>> = MutableList(rows) { MutableList(columns) { Cell.EMPTY } }
-    private val observers: MutableSet<FieldObserver> = mutableSetOf()
+abstract class TicTacToe(protected val rows: Int, protected val columns: Int) {
+    abstract fun getCell(position: TicTacToe.Position): Cell
 
-    override fun getCell(position: TicTacToe.Position): Cell {
-        return field[position.row][position.column]
+    abstract fun setCell(position: TicTacToe.Position, cell: Cell): Boolean
+    abstract val field: List<List<Cell>>
+
+    class Base(row: Int, columns: Int) : TicTacToe(row, columns), ObservableField {
+        override val field: MutableList<MutableList<Cell>> = MutableList(rows) { MutableList(columns) { Cell.EMPTY } }
+        private val observers: MutableSet<FieldObserver> = mutableSetOf()
+
+        override fun getCell(position: TicTacToe.Position): Cell {
+            return field[position.row][position.column]
+        }
+
+        override fun setCell(position: TicTacToe.Position, cell: Cell): Boolean {
+            field[position.row][position.column] = cell
+            observers.forEach { it.onFieldChanged(field) }
+            return true
+        }
+
+        override fun subscribe(observer: FieldObserver) {
+            observers.add(observer)
+        }
+
+        override fun unsubscribe(observer: FieldObserver) {
+            observers.remove(observer)
+        }
     }
 
-    override fun setCell(position: TicTacToe.Position, cell: Cell): Boolean {
-        field[position.row][position.column] = cell
-        observers.forEach { it.onFieldChanged(field) }
-        return true
-    }
-
-    fun subscribe(fieldObserver: FieldObserver) {
-        observers.add(fieldObserver)
-    }
-
-    fun unsubscribe(fieldObserver: FieldObserver) {
-        observers.remove(fieldObserver)
-    }
 
     inner class Position(
         val row: Int,
